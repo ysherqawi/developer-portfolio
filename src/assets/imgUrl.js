e36897c1fb4bad46649879a3ae92735143f2b48a
ysherqawi <ysherqawi@gmail.com>
2020-09-05 22:33:51 +0300

    added optimaized images

--- src/assets/imgUrl.js
+++ src/assets/imgUrl.js
@@ -1,37 +1,26 @@
 //Pages
-const homePageImg =
-  'https://lh3.googleusercontent.com/A1ZLOsJEGGv3K7gbSoYRzCY1sOcrOmGh952o28nZo485e5Ti73ZEwpgJopT1SSTNnpoeIh1eFpurBpioyLZo_hdcNQV9I-kuvSaMDRSnevbfX5s9Unik80PTwrNlCK1GUwQbEz6H=w2400';
+import homePageImg from '../assets/img/developer.gif';
 
-const aboutPageImg =
-  'https://lh3.googleusercontent.com/9H4gHLqfRVXmQ98j8QvxrWPHT4Gi3BIw97IZKf9WzT1QxCMUH_1sn671fk5Xak_ZWmZeuqgRukTn9gVTriHGEzseB9flY3NqM2E9t8udRUxhyoyLQvgRsggf4PEVcrjkkNfa7dsC=w2400';
+import aboutPageImg from '../assets/img/about.gif';
 
-const skillsPageImg =
-  'https://lh3.googleusercontent.com/2CJ0VwHI8DEGbRtKhiubH_573bHQHp5-VHiuuaFcYVnpYEybCTmsEWNFOlWyzAm0tw5WtCRnQ-vLuOD0bR9_ELQoFmLRV_1bnFkh-bpTmORyGpE0e2ZIP9KDCCb4eQ8WO7EQfPCF=w2400';
+import skillsPageImg from '../assets/img/skills.gif';
 
-const notFoundImg =
-  'https://lh3.googleusercontent.com/Xov5PgZCmeR8C4ZY5szfNHaZoeQinXq4caEobLhEhegK4Z_F-aURo_xuiBIRoUSj-TearNL1JrV8TWnH5cv3WXPhvDcdcZAReNs_-FjgREtMdTBbqcaRKZIwqeBdnIDzrFXAmVZ0=w2400';
+import notFoundImg from '../assets/img/notfound.gif';
 
 //Projects
-const defaultProjectImg =
-  'https://lh3.googleusercontent.com/tUXVqwPZVY5Cxd64fQfhJZNGXjj-XHOcd_1_JOaFaG8EADUcHaq4x9qTGKPaAZlylYgPzmQ80oadqLiTKkBb6L5rbe13NL0FhxCvHiOPh0rjJtYiubLcTLnrgK0xOId1ZynuLttp=w2400';
+import defaultProjectImg from '../assets/img/default-project.jpg';
 
-const devConnectorImg =
-  'https://lh3.googleusercontent.com/yGLHcbpuzjiw1iqXmC0N-_AoHPpgQZZN_qQuNdEh2HS8CQy22rG7EbF6xvxLC3O7m19msqAvesEqwqamBnkwRGRp2_LZhctJyCLz_TrjHI6S3XNb6rjy_Hcw-5Z3xqm-i9wIE_zQ=w2400';
+import devConnectorImg from '../assets/img/devconnector-project.jpg';
 
-const devCamperImg =
-  'https://lh3.googleusercontent.com/Nn2qnK5B6EgVQXhOtF86ZVYRsOvvXl8kbu75AdOjqcrqAkCnrqnK4tS_7uZDDFPXLGQFRVEuPH_UwUX4DRNy9a-0j5w8amLEdnQKyiYUATsVqKY2I5QvM5aEWmfY159EhcpvPcz0=w2400';
+import devCamperImg from '../assets/img/devcamper-project.jpg';
 
-const ecommerceImg =
-  'https://lh3.googleusercontent.com/A7fjnPyns2mJRXu35-HLvf6Jy4H8VrrtJQePG8JB58eFtiVjhKChMYInWqNrdp29AOd1yGaJG9rCkeZqQ5t-QnQBS4vQnhlCUvOxw68eP3QZkPn3AqsiS3atpf4oyR1fD7HvIjqT=w2400';
+import ecommerceImg from '../assets/img/ecommerce-project.jpg';
 
-const movieAppImg =
-  'https://lh3.googleusercontent.com/_IiA1JyDwDZKQ2DHv1HpOuo9o8U-mGZhfDXNuaxsJ61G4J_rGwrhpy3fMw32hI8WptqCjewP_PLHI0JCutw6MPCZsnnsPf62Bo2fEOMWbuovj_cVHWxINrAL_UscmNjdxZDkCxXo=w2400';
+import movieAppImg from '../assets/img/movieapp-project.jpg';
 
-const githubFinderImg =
-  'https://lh3.googleusercontent.com/_qBwcSW-MYIfU43D59i8-htUSlmCa_d5zgNv4a_VrkCpMlPF1IiThbDWlfOVABOmA7nBguD1T2HMYj-SPXfP9toO7syCUpC9ZmvCISinjqavE9Cby68JgFR_6dasx0XMogjGWJ0l=w2400';
+import githubFinderImg from '../assets/img/github-project.jpg';
 
-const nodeChatAppImg =
-  'https://lh3.googleusercontent.com/-barHirJsFJ9vJaYBzNFop28SSFkg5mnb9xJYrHY7yn9bWkfo50Gm39GqXKNZ4Szodt2tXStHc-vjcSOE_fiTxdl0dUoEUGV8VH4HnCWsmHphg9OZFJhf5OAWLPyJHrnH8RvoUWA=w2400';
+import nodeChatAppImg from '../assets/img/chat-project.jpg';
 
 export {
   homePageImg,
