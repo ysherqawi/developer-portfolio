e9a8c584b5576d95acc6e2c6660bde29743bffa1
ysherqawi <ysherqawi@gmail.com>
2021-03-02 20:57:21 +0200

    edited home component

--- src/components/pages/Landing/Home.js
+++ src/components/pages/Landing/Home.js
@@ -1,35 +1,35 @@
 import React from 'react';
 import { Animated } from 'react-animated-css';
-import Typewriter from 'typewriter-effect';
+// import Typewriter from 'typewriter-effect';
 import { homePageImg } from '../../../assets/imgUrl';
 import '../../styles/Home/Home.css';
 
 const Home = () => (
   <div className='mw8 br3 pt3 home-layout'>
     <Animated
-      animationIn='fadeInRightBig'
-      animationOut='zoomOutDown'
+      animationIn='fadeIn'
+      animationOut='fadeOut'
       animationInDelay={500}
     >
       <h1 className='tc f1 mb4 header-text'>
-        <Typewriter
+        {/* <Typewriter
           options={{
-            strings: [
-              `Hello <i class="far fa-grin"/>`,
-              `I'm Yasser Sherqawi`,
-              `I'm a Web Developer`,
-            ],
+            strings: [`Hi all <i class="far fa-grin"/>`, `I'm Yasser`],
             autoStart: true,
             loop: true,
           }}
-        />
+        /> */}
+        Hi all, I'm Yasser
+        <span role='img' aria-label='waving hand' className='wave-emoji'>
+          👋
+        </span>
       </h1>
     </Animated>
 
     <Animated
-      animationIn='shake'
-      animationOut='zoomOutDown'
-      animationInDelay={1400}
+      animationIn='fadeIn'
+      animationOut='fadeOut'
+      animationInDelay={500}
     >
       <img className='landing-img' src={homePageImg} alt='Developer Gif' />
     </Animated>
