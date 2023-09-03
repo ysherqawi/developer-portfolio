a36ed3006ad4f1c3652c4b1e43ddbd955b5e3cc1
ysherqawi <ysherqawi@gmail.com>
2023-09-03 11:27:13 +0300

    updated projects data

--- src/assets/imgUrl.js
+++ src/assets/imgUrl.js
@@ -1,32 +1,35 @@
 //Pages
-import homePageImg from '../assets/img/developer.gif';
+import homePageImg from "../assets/img/developer.gif";
 
-import aboutPageImg from '../assets/img/about.gif';
+import aboutPageImg from "../assets/img/about.gif";
 
-import skillsPageImg from '../assets/img/skills.gif';
+import skillsPageImg from "../assets/img/skills.gif";
 
-import notFoundImg from '../assets/img/notfound.gif';
+import notFoundImg from "../assets/img/notfound.gif";
 
-import SpinnerImg from './spinner.gif';
+import SpinnerImg from "./spinner.gif";
 
 //Projects
-import defaultProjectImg from '../assets/img/default-project.jpg';
+import defaultProjectImg from "../assets/img/default-project.jpg";
 
-import devConnectorImg from '../assets/img/devconnector-project.jpg';
+import devConnectorImg from "../assets/img/devconnector-project.jpg";
 
-import devCamperImg from '../assets/img/devcamper-project.jpg';
+import devCamperImg from "../assets/img/devcamper-project.jpg";
 
-import proshop from '../assets/img/proshop-project.jpg';
+import proshop from "../assets/img/proshop-project.jpg";
 
-import movieAppImg from '../assets/img/movieapp-project.jpg';
+import movieAppImg from "../assets/img/movieapp-project.jpg";
 
-import nodeChatAppImg from '../assets/img/chat-project.jpg';
+import realtorAppImg from "../assets/img/realtorapp-project.jpg";
 
-import donewithitImg from '../assets/img/donewithit-project.jpg';
+import nodeChatAppImg from "../assets/img/chat-project.jpg";
+
+import donewithitImg from "../assets/img/donewithit-project.jpg";
 
 export {
   homePageImg,
   aboutPageImg,
+  realtorAppImg,
   skillsPageImg,
   notFoundImg,
   SpinnerImg,
