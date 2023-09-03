25b5ac3551c7cc0695dc129967f40ffa2baea9b1
ysherqawi <ysherqawi@gmail.com>
2023-09-03 11:57:46 +0300

    updated skills component

--- src/components/pages/Skills/BackSkills.js
+++ src/components/pages/Skills/BackSkills.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import { Animated } from 'react-animated-css';
-import backIcon from '../../../assets/backend.png';
-import '../../styles/Skills/Skills.css';
+import React from "react";
+import { Animated } from "react-animated-css";
+import backIcon from "../../../assets/backend.png";
+import "../../styles/Skills/Skills.css";
 
 const BackSkills = () => (
-  <Animated animationIn='fadeIn' animationOut='fadeOut' animationInDelay={500}>
+  <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDelay={500}>
     <article
-      className='skills-card 
-        mw5 mh2 grow bg-white br3 pa3 pa4-ns mv3 ba b--black-10'
+      className="skills-card 
+        mw5 mh2 grow bg-white br3 pa3 pa4-ns mv3 ba b--black-10"
     >
-      <div className='tc'>
+      <div className="tc">
         <h3>
-          <img src={backIcon} class='skills-icon' alt='backend logo' /> Backend
+          <img src={backIcon} class="skills-icon" alt="backend logo" /> Backend
         </h3>
-        <hr className='mw3 bb bw1 b--black-10' />
+        <hr className="mw3 bb bw1 b--black-10" />
       </div>
-      <div className='cursor'>
-        <span className='f5 db black link hover-dark-green pv1'>
-          <i class='fab fa-node' /> Node.js
+      <div className="cursor">
+        <span className="f5 db black link hover-dark-green pv1">
+          <i class="fab fa-node" /> Node.js
         </span>
-        <span className='f5 db black link hover-gray pv1'>
-          <i className='fab fa-node-js' /> Express.js
+        <span className="f5 db black link hover-gray pv1">
+          <i className="fab fa-node-js" /> Express.js
         </span>
-        <span className='f5 db black link hover-gray pv1'>
-          <i className='fab fa-node-js' /> Nest.js
+        <span className="f5 db black link hover-dark-red pv1">
+          <i className="fab fa-node-js" /> Nest.js
         </span>
-        <span className='f5 db black link hover-green pv1'>
-          <i class='fab fa-envira' /> MongoDB
+        <span className="f5 db black link hover-green pv1">
+          <i class="fab fa-envira" /> MongoDB
         </span>
-        <span className='f5 db black link hover-dark-red pv1'>
-          <i class='fas fa-database' /> Mongoose
+        <span className="f5 db black link hover-dark-red pv1">
+          <i class="fas fa-database" /> Mongoose
         </span>
-        <span className='f5 db black link hover-dark-blue pv1'>
-          <i class='fas fa-database' /> PostgreSQL
+        <span className="f5 db black link hover-dark-blue pv1">
+          <i class="fas fa-database" /> PostgreSQL
         </span>
-        <span className='f5 db black link hover-dark-blue pv1'>
-          <i class='fas fa-database' /> MySQL
+        <span className="f5 db black link hover-dark-blue pv1">
+          <i class="fas fa-database" /> MySQL
         </span>
       </div>
     </article>
--- src/components/pages/Skills/FrontSkills.js
+++ src/components/pages/Skills/FrontSkills.js
@@ -1,43 +1,43 @@
-import React from 'react';
-import { Animated } from 'react-animated-css';
-import frontIcon from '../../../assets/frontend.png';
-import '../../styles/Skills/Skills.css';
+import React from "react";
+import { Animated } from "react-animated-css";
+import frontIcon from "../../../assets/frontend.png";
+import "../../styles/Skills/Skills.css";
 
 const FrontSkills = () => (
-  <Animated animationIn='fadeIn' animationOut='fadeOut' animationInDelay={500}>
-    <article className='skills-card mw5 mh2 grow bg-white br3 pa3 pa4-ns mv3 ba b--black-10'>
-      <div className='tc'>
+  <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDelay={500}>
+    <article className="skills-card mw5 mh2 grow bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+      <div className="tc">
         <h3>
           <img
             src={frontIcon}
-            className='skills-icon'
-            alt='front-end space logo'
-          />{' '}
+            className="skills-icon"
+            alt="front-end space logo"
+          />{" "}
           Frontend
         </h3>
-        <hr className='mw3 bb bw1 b--black-10' />
+        <hr className="mw3 bb bw1 b--black-10" />
       </div>
-      <div className='cursor'>
-        <span className='f5 db black link hover-orange pv1'>
-          <i className='fab fa-html5' /> HTML
+      <div className="cursor">
+        <span className="f5 db black link hover-orange pv1">
+          <i className="fab fa-html5" /> HTML
         </span>
-        <span className='f5 db black link hover-dark-blue pv1'>
-          <i className='fab fa-css3-alt' /> CSS
+        <span className="f5 db black link hover-dark-blue pv1">
+          <i className="fab fa-css3-alt" /> CSS
         </span>
-        <span className='f5 db black link hover-yellow pv1'>
-          <i className='fab fa-js-square' /> JavaScript (ES6)
+        <span className="f5 db black link hover-yellow pv1">
+          <i className="fab fa-js-square" /> JavaScript
         </span>
-        <span className='f5 db black link hover-pink pv1'>
-          <i className='fab fa-sass' /> SASS
+        <span className="f5 db black link hover-pink pv1">
+          <i className="fab fa-sass" /> SASS
         </span>
-        <span className='f5 db black link hover-purple pv1'>
-          <i className='fab fa-bootstrap' /> Bootstrap
+        <span className="f5 db black link hover-purple pv1">
+          <i className="fab fa-bootstrap" /> Bootstrap
         </span>
-        <span className='f5 db black link hover-blue pv1'>
-          <i className='fab fa-react' /> React.js
+        <span className="f5 db black link hover-blue pv1">
+          <i className="fab fa-react" /> React.js
         </span>
-        <span className='f5 db black link hover-light-blue pv1'>
-          <i className='fab fa-react' /> React Native
+        <span className="f5 db black link hover-light-blue pv1">
+          <i className="fab fa-react" /> React Native
         </span>
       </div>
     </article>
